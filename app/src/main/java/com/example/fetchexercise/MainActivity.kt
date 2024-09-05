96183af3647ca995325cf949fa57869ff3ca578f
Cole Rasmussen <colewrasmussen@gmail.com>
2024-09-05 14:43:51 -0400

    updated commit

--- app/src/main/java/com/example/fetchexercise/MainActivity.kt
+++ app/src/main/java/com/example/fetchexercise/MainActivity.kt
@@ -2,29 +2,37 @@ package com.example.fetchexercise
 
 import android.os.Bundle
 import android.util.Log
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+
 import androidx.compose.runtime.*
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import com.example.fetchexercise.ui.theme.FetchExerciseTheme
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+
+//retrofit imports
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -33,32 +41,31 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FetchExerciseApp()
+            FetchExercise()
         }
     }
 }
 
 @Composable
-fun FetchExerciseApp() {
-    var fItems by remember { mutableStateOf(emptyList<Item>()) }
+fun FetchExercise() {
+    var readItems by remember { mutableStateOf(emptyList<Item>()) }
     var fCategories by remember { mutableStateOf(setOf<Int>()) }
 
     LaunchedEffect(Unit) {
         RetrofitClient.instance.getItems().enqueue(object : Callback<List<Item>> {
             override fun onResponse(call: Call<List<Item>>, response: Response<List<Item>>) {
                 if (response.isSuccessful) {
-                    fItems = response.body()?.filter { !it.name.isNullOrEmpty() }
-                        ?.sortedWith(compareBy<Item> { it.listId }
-                            .thenBy { extractNumber(it.name ?: "") }) ?: emptyList()
+                    val items = response.body()?.filter { !it.name.isNullOrEmpty() }
+                    val sItems = items?.sortedWith(compareBy<Item> { it.listId }
+                        .thenBy { extractNumber(it.name ?: "") }) ?: emptyList()
 
-                    Log.d("MainActivity", "Filtered and Sorted Items: $fItems")
-                } else {
-                    Log.e("MainActivity", "Error: ${response.code()}")
+                    readItems = sItems
                 }
             }
 
             override fun onFailure(call: Call<List<Item>>, t: Throwable) {
-                Log.e("MainActivity", "API call failed: ${t.message}")
+                //For testing purposes
+                Log.e("MainActivity", "API CALL FAILED: ${t.message}")
             }
         })
     }
@@ -73,13 +80,12 @@ fun FetchExerciseApp() {
                     .padding(16.dp),
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                (1..4).forEach { listId ->
-                    val filteredItems = fItems.filter { it.listId == listId }
-
+                for (listId in 1..4) {
+                    val fItems = readItems.filter { it.listId == listId }
                     item {
                         CategorySection(
                             listId = listId,
-                            newItems = filteredItems,
+                            nItems = fItems,
                             isExpanded = fCategories.contains(listId),
                             onToggleExpand = {
                                 fCategories = if (fCategories.contains(listId)) {
@@ -96,13 +102,13 @@ fun FetchExerciseApp() {
     }
 }
 
-val exo2FontFamily = FontFamily(
+val customFontFamily = FontFamily(
     Font(R.font.exo2regular, FontWeight.Normal),
     Font(R.font.exo2bold, FontWeight.Bold)
 )
 
 @Composable
-fun CategorySection(listId: Int, newItems: List<Item>, isExpanded: Boolean, onToggleExpand: () -> Unit) {
+fun CategorySection(listId: Int, nItems: List<Item>, isExpanded: Boolean, onToggleExpand: () -> Unit) {
     Column(modifier = Modifier.fillMaxWidth()) {
         Box(
             modifier = Modifier
@@ -120,8 +126,7 @@ fun CategorySection(listId: Int, newItems: List<Item>, isExpanded: Boolean, onTo
         ) {
             Text(
                 text = "List ID: $listId",
-                //color = Color.Red,
-                fontFamily = exo2FontFamily,
+                fontFamily = customFontFamily,
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center,
@@ -131,7 +136,7 @@ fun CategorySection(listId: Int, newItems: List<Item>, isExpanded: Boolean, onTo
 
         if (isExpanded) {
             Column(modifier = Modifier.padding(start = 16.dp)) {
-                newItems.forEach { item ->
+                nItems.forEach { item ->
                     Column {
                         Row(
                             modifier = Modifier
@@ -140,16 +145,15 @@ fun CategorySection(listId: Int, newItems: List<Item>, isExpanded: Boolean, onTo
                         ) {
                             Text(
                                 text = "ID: ${item.id}",
-                                fontFamily = exo2FontFamily,
+                                fontFamily = customFontFamily,
                                 modifier = Modifier
                                     .weight(1f)
                                     .alignByBaseline(),
-                                //fontWeight = FontWeight.Bold
                             )
 
                             Text(
                                 text = "Name: ${item.name}",
-                                fontFamily = exo2FontFamily,
+                                fontFamily = customFontFamily,
                                 modifier = Modifier
                                     .weight(1f)
                                     .fillMaxHeight()
@@ -170,7 +174,10 @@ fun CategorySection(listId: Int, newItems: List<Item>, isExpanded: Boolean, onTo
 }
 
 fun extractNumber(name: String): Int {
-    return name.split(" ").lastOrNull()?.toIntOrNull() ?: 0
+    val parts = name.split(" ")
+    val lastPart = parts.lastOrNull()
+    val number = lastPart?.toIntOrNull() ?: 0
+    return number
 }
 
 @Preview(showBackground = true)
