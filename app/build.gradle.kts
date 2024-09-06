283e88a6d443c96c07caeaf1e5593b8f769ab87b
Cole Rasmussen <colewrasmussen@gmail.com>
2024-09-06 10:57:02 -0400

    updated commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,9 +60,9 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.google.code.gson:gson:2.8.6")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.gson)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
